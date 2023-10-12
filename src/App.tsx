169a77243fca33b0175a94d7245dfdf30fe65399
Horlan Vicenssotti <88784529+hrVicenssotti@users.noreply.github.com>
2023-10-12 19:34:44 +0000

    config style initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-function App() {
-   return (
-      <>
-         <h1>Myself Project</h1>
-      </>
-   );
-}
+import { styled } from 'styled-components';
+import React from 'react';
+
+export const AppContainer = styled.div(({ theme }) => ({
+    backgroundColor: theme.palette.secondary.main,
+    boxSizing: 'border-box',
+    minHeight: '100vh',
+    padding: '8px',
+}));
+
+export const App: React.FC = () => {
+    return <AppContainer></AppContainer>;
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider } from 'styled-components';
+
+import { theme } from './assets/styles/theme';
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-   <React.StrictMode>
-      <App />
-   </React.StrictMode>
+    <StrictMode>
+        <ThemeProvider theme={theme}>
+            <App />
+        </ThemeProvider>
+    </StrictMode>
 );
