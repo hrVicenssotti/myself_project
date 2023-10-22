a98c1ae9bedca82ab825e36d6ae9f9d3cb22bf1e
Horlan Vicenssotti <88784529+hrVicenssotti@users.noreply.github.com>
2023-10-22 23:13:41 +0000

    my about

--- src/pages/Myself/index.tsx
+++ src/pages/Myself/index.tsx
@@ -1,10 +1,11 @@
-import { Box } from '@mui/material';
+import { Box, Link, Typography } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 
 import { useStyles } from './styles';
-import { ButtomToTop } from '../../components';
+import { ButtomToTop, IconGithub, IconLinkedin } from '../../components';
 import { NavBar } from './components';
 import myselfImage from '../../assets/images/myself.jpg';
+import { Map } from '@mui/icons-material';
 
 export const Myself: React.FC = () => {
     const classes = useStyles();
@@ -26,11 +27,44 @@ export const Myself: React.FC = () => {
             <NavBar scrollOffTop={offTopScroll} />
             <Box component={'main'} className={classes.main}>
                 <Box id={'home'} className={`${classes.container} ${classes.diffAnchor}`}>
-                    <Box>
+                    <Box className={classes.start}>
                         <Box>
-                            <h1>Horlan Vicenssotti</h1>
+                            <Box>
+                                <Typography variant={'body1'}>Olá mundo,</Typography>
+                                <Typography variant={'h1'}>Eu sou Horlan Vicenssotti 👋,</Typography>
+                                <Typography variant={'body1'}>
+                                    Sou desenvolvedor FullStack com experiencia em aplicações WEB usando ReactJS,
+                                    NodeJS, PHP e Typescript. Com habilidade em desenvolver aplicações de fácil uso,
+                                    responsivo e escalável. Com responsabilidade para entregar códigos de alta
+                                    qualidade, fácil manutenção e seguindo os padrões de mercado.
+                                </Typography>
+                            </Box>
+                            <Box sx={{ margin: '1.25rem 0' }}>
+                                <Link
+                                    target={'_blank'}
+                                    href={'https://maps.app.goo.gl/zNcNqesXwb8EcAKs6'}
+                                    sx={{
+                                        display: 'flex',
+                                        alignItems: 'center',
+                                        gap: '1rem',
+                                        color: 'inherit',
+                                        textDecoration: 'inherit',
+                                    }}
+                                >
+                                    <Map />
+                                    <Typography variant={'body1'}>Brasil</Typography>
+                                </Link>
+                            </Box>
+                            <Box sx={{ display: 'flex', gap: '1rem' }}>
+                                <Link target={'_blank'} href={'https://github.com/hrVicenssotti'}>
+                                    <IconGithub sx={{ fontSize: '2.5rem' }} />
+                                </Link>
+                                <Link target={'_blank'} href={'https://www.linkedin.com/in/hrvicenssotti/'}>
+                                    <IconLinkedin sx={{ fontSize: '2.5rem' }} />
+                                </Link>
+                            </Box>
                         </Box>
-                        <Box>
+                        <Box sx={{ justifySelf: 'center' }}>
                             <img className={classes.myselfImage} src={myselfImage} alt={'Minha imagem'} />
                         </Box>
                     </Box>
--- src/pages/Myself/styles.ts
+++ src/pages/Myself/styles.ts
@@ -10,21 +10,32 @@ export const useStyles = makeStyles((theme: Theme) => ({
     },
     main: {
         boxSizing: 'content-box',
-
-        '& h1': {
-            margin: 0,
-        },
     },
     container: {
         padding: '1rem',
+
+        '& h1': {
+            fontWeight: 'bold',
+            fontSize: '2.5rem',
+            margin: '0.25rem 0',
+        },
+        '& p': {
+            fontSize: '1.25rem',
+        },
     },
     diffAnchor: {
         paddingTop: '4rem',
     },
     myselfImage: {
-        width: '182px',
-        height: '182px',
+        width: '240px',
+        height: '240px',
         borderRadius: '50%',
         boxShadow: '0 0 15px -7px #000000cc',
+    },
+    start: {
+        display: 'grid',
+        gridTemplateColumns: '1fr 368px',
+        alignItems: 'center',
+        minHeight: '400px',
     },
 }));
